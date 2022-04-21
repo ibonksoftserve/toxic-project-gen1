b33f7cd4c91b8c2b48e752e21c2703fc55af5178
Ivan Bonk <ibonk@softserveinc.com>
2022-04-22 00:01:27 +0300

    ib/ fix frontend dockerfile | recreate package-lock | add request-logger

--- backend/app/loaders/express.ts
+++ backend/app/loaders/express.ts
@@ -3,12 +3,15 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import { routes } from '../modules';
 import { configs } from '../config';
+import { requestLogger } from './request-logger';
 
 export const initExpress = async (app: Express): Promise<void> => {
 
   app.use(cors());
   app.use(express.json());
   app.use(cookieParser());
-  app.use(configs.apiPrefix, routes());
 
+  app.use(requestLogger);
+
+  app.use(configs.apiPrefix, routes());
 }

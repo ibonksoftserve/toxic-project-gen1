5ceb79b265b969201acefba9f7dcc90a565e6f63
Ivan Bonk <ibonk@softserveinc.com>
2022-05-25 19:14:12 +0300

    test app build

--- test_jenkins.js
+++
@@ -1,9 +0,0 @@
-(() => {
-  console.log("Start working on some kind of shit");
-
-  for (let i = 0; i < 10; i++) {
-    console.log("Some iterations => ", i);
-  }
-
-  console.log("Stop working on some kind of shit");
-})();

35d1929845c3141382f0f7d4b5da4d8ffad80c28
Ivan Bonk <ibonk@softserveinc.com>
2022-05-26 01:15:43 +0300

    fix error handling

--- backend/app/middlewares/errors-handler.ts
+++ backend/app/middlewares/errors-handler.ts
@@ -1,10 +1,12 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { ApiError, ServerError } from '../core/api.errors';
 
-export const errorsHandler = (err: Error, req: Request, res: Response) => {
+export const errorsHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof ApiError) {
     ApiError.handleError(err, res);
   } else {
     ApiError.handleError(new ServerError(err), res);
   }
+
+  next();
 };
